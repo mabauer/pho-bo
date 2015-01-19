1ece52e8990e63b909b76c76881ae39e9a4442b0
Markus Bauer <mkbauer@web.de>
2015-01-19 18:41:38 +0100

    Portfolio: multi-column, responsive layout, hover effects

--- content-portfolio.php
+++ content-portfolio.php
@@ -5,15 +5,21 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('portfolio-element'); ?>>
-
-	<div class="entry-content clearfix">
+		
 		<?php if ( has_post_thumbnail() ): ?>
 			<a href="<?php the_permalink(); ?>" class="portfolio-thumb"><?php the_post_thumbnail('index-thumb'); ?></a>
 		<?php endif; ?>
 
-		<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'baylys' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-	
-				<?php the_excerpt(); ?>
+	<header class="entry-header clearfix">
+		
+		<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'pho' ), 
+			the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+
+		<div class="entry-meta">
+			<?php pho_posted_on(); ?>
+		</div><!-- .entry-meta -->
+	</header>
+	<div class="entry-content clearfix">	
 	</div><!-- end .entry-content -->
 
 </article><!-- end post -<?php the_ID(); ?> -->
