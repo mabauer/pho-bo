df7e12ffd714f9b60860f058ce3dcfda640a1629
Markus Bauer <mkbauer@web.de>
2016-09-02 10:32:48 +0200

    Updates to new API in Wordpress 4.x

--- inc/social-widget.php
+++ inc/social-widget.php
@@ -21,33 +21,33 @@ class pho_sociallinks extends WP_Widget {
 		'tumblr' => array('Tumblr', 'fa-tumblr'),
 		'foursquare' => array('Foursquare', 'fa-foursquare'),
 		'rss' => array('Flickr', 'fa-rss'),
-		);		
+		);
 
 	function pho_sociallinks() {
 		$widget_ops = array('description' => __( 'Show icons with links to your social profiles' , 'pho') );
 
-		parent::WP_Widget(false, __('Social Links (pho)', 'pho'),$widget_ops);
+		parent::__construct(false, __('Social Links (pho)', 'pho'),$widget_ops);
 	}
 
 	function widget($args, $instance) {
 		extract( $args );
 		$title = $instance['title'];
-		
-		
-		
+
+
+
 		echo $before_widget; ?>
 		<?php if($title != '')
 			echo '<h3 class="widget-title"><span>'. esc_html($title) .'</span></h3>'; ?>
 
         <ul class="sociallinks">
 			<?php
-			
+
 			foreach ($this->sociallinks as $key => $linkprops) {
-			
+
 				$link = ! empty( $instance[$key] ) ? esc_attr($instance[$key]) : '';
-				
+
 				if ($link != '') {
-					echo '<li><a href="'.esc_url( $link ).'" title="' . __( $linkprops[0], 'pho' ) . '">' 
+					echo '<li><a href="'.esc_url( $link ).'" title="' . __( $linkprops[0], 'pho' ) . '">'
 						. '<i class="fa ' . $linkprops[1] .'"></i>' . '</a></li>';
 				}
 			}
@@ -71,24 +71,24 @@ class pho_sociallinks extends WP_Widget {
 		$title = ! empty( $instance['title'] ) ? esc_attr($instance['title']) : '';
 
 		echo '<label for="' . $this->get_field_id('title') . '">' . __('Title:','pho') . '</label>';
-		echo '<input type="text" name="' . $this->get_field_name('title') . '" value="' 
-			. esc_attr($title) . '" class="widefat" id="' . $this->get_field_id('title') . '"/>';		
-				
-		/* 
+		echo '<input type="text" name="' . $this->get_field_name('title') . '" value="'
+			. esc_attr($title) . '" class="widefat" id="' . $this->get_field_id('title') . '"/>';
+
+		/*
 		<p>
             <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:','pho'); ?></label>
             <input type="text" name="<?php echo $this->get_field_name('title'); ?>" value="<?php echo esc_attr($title); ?>" class="widefat" id="<?php echo $this->get_field_id('title'); ?>" />
         </p>
         */
-        
+
 		foreach ($this->sociallinks as $key => $linkprops) {
-			
+
 			$link = ! empty( $instance[$key] ) ? esc_attr($instance[$key]) : '';
-			echo '<label for="' . $this->get_field_id('link') . '">' . __($linkprops[0], 'pho') 
+			echo '<label for="' . $this->get_field_id('link') . '">' . __($linkprops[0], 'pho')
 					. /* '<i class="fa ' . $linkprops[1] . '"></i>' . */ '</label>';
 
-			echo '<input type="text" name="' . $this->get_field_name($key) . '" value="' 
-					. esc_attr($link) . '" class="widefat" id="' . $this->get_field_id($key) . '"/>';		
+			echo '<input type="text" name="' . $this->get_field_name($key) . '" value="'
+					. esc_attr($link) . '" class="widefat" id="' . $this->get_field_id($key) . '"/>';
 			}
 
 	} // form
