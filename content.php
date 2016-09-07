3b77d4a29a7e80493db9ea3938611ee6c252b187
Markus Bauer <mkbauer@web.de>
2016-09-07 13:58:34 +0200

    Display comment link for a post on index pages only if there are any comments.

--- content.php
+++ content.php
@@ -5,17 +5,17 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
- 
+
  	<?php
- 	
+
  		$featured_tag = get_theme_mod('pho_featured_post_tag');
 		if ('' == $featured_tag) {
 			$featured_tag = 'featured';
-		} 
+		}
 
 		// On larger screens, featured posts will be displayed with large images,...
-		// ... normal ones get side teasers. 
-		$teaser = 'no-teaser';	
+		// ... normal ones get side teasers.
+		$teaser = 'no-teaser';
  		if (has_post_thumbnail()) {
  			if (has_tag($featured_tag) || is_sticky()) {
  				$teaser = 'large-teaser';
@@ -23,15 +23,15 @@
  			else {
  				$teaser = 'side-teaser';
  			}
- 		} 
+ 		}
 	?>
-		
+
     <div class="index-box clear">
-    
-    
+
+
     <?php
 
-		// Featured image                
+		// Featured image
 		if (has_post_thumbnail()) {
 			echo '<div class="thumbnail-box clear ' . $teaser . '">';
 			echo '<a href="' . get_permalink() . '" title="' . __('Read ', 'pho') . get_the_title() . '" rel="bookmark">';
@@ -39,8 +39,8 @@
 			the_post_thumbnail();
 
 			// For large teaser images, get a 'responsive' image, for side teasers the small one suffices.
-			/* 
-			if ('large-teaser' == $teaser) { 
+			/*
+			if ('large-teaser' == $teaser) {
 				the_post_thumbnail();
 			}
 			else {
@@ -50,8 +50,8 @@
 			echo '</a>';
 			echo '</div>';
 		}
-	?>		
-	   
+	?>
+
 	<div class ="entry-box <?php echo $teaser ?>">
 	<header class="entry-header clear">
         <?php
@@ -68,10 +68,10 @@
 				echo '<div class="category-list">' . $category_list . '</div>';
 			}
         ?>
-        
+
 		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
 
-		
+
 	</header><!-- .entry-header -->
 
         <?php
@@ -80,41 +80,41 @@
             the_content( __( '', 'pho' ) );
             echo '</div>';
             echo '<footer class="entry-footer continue-reading">';
-            echo '<a href="' . get_permalink() . '" title="' . _x('Read ', 'First part of "Read *article title* in title tag of Read more link', 'pho') . get_the_title() . '" rel="bookmark">' 
+            echo '<a href="' . get_permalink() . '" title="' . _x('Read ', 'First part of "Read *article title* in title tag of Read more link', 'pho') . get_the_title() . '" rel="bookmark">'
             	. __('Read <span aria-hidden="true">the article</span>', 'pho') . '<i class="fa fa-chevron-circle-right"></i><span class="screen-reader-text"> ' . get_the_title() . '</span></a>';
             echo '</footer><!-- .entry-footer -->';
         } else { ?>
             <div class="entry-content">
-                
-                <?php 
+
+                <?php
                 $pho_archive_content = get_option( 'archive_setting' );
                 if ( $pho_archive_content == 'content' ) {
                     the_content( __( '', 'pho' ) );
                 } else {
-                    the_excerpt(); 
+                    the_excerpt();
                 }
                 ?>
                 <div class="continue-reading">
-					<?php 
+					<?php
 						if ( $pho_archive_content == 'content' ) {
-							echo '<a href="' . get_permalink() . '" title="' . _x('Read ', 'First part of "Read *article title* in title tag of Read more link', 'pho') . get_the_title() . '" rel="bookmark">' 
+							echo '<a href="' . get_permalink() . '" title="' . _x('Read ', 'First part of "Read *article title* in title tag of Read more link', 'pho') . get_the_title() . '" rel="bookmark">'
 								. __('Read <span aria-hidden="true">the article</span>', 'pho') . '<i class="fa fa-chevron-circle-right"></i><span class="screen-reader-text"> ' . get_the_title() . '</span></a>';
 						} else {
-							echo '<a href="' . get_permalink() . '" title="' . __('Continue Reading ', 'pho') . get_the_title() . '" rel="bookmark">' 
-								. __('More', 'pho') . '<i class="fa fa-chevron-circle-right"></i><span class="screen-reader-text"> ' . get_the_title() . '</span></a>'; 
+							echo '<a href="' . get_permalink() . '" title="' . __('Continue Reading ', 'pho') . get_the_title() . '" rel="bookmark">'
+								. __('More', 'pho') . '<i class="fa fa-chevron-circle-right"></i><span class="screen-reader-text"> ' . get_the_title() . '</span></a>';
 						}
 					?>
 				</div> <!-- .continue-reading -->
 			</div> <!-- .entry-content -->
-            
-            
-						
+
+
+
 			<footer class="entry-footer">
                 <?php if ( 'post' == get_post_type() ) : ?>
 				<div class="entry-meta">
 					<?php pho_posted_on(); ?>
 					<?php
-					if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) {
+					if ( ! post_password_required() && ( comments_open() && '0' != get_comments_number() ) ) {
 						echo '<span class="comments-link">';
 						comments_popup_link( __( 'Leave a comment', 'pho' ), __( '1 Comment', 'pho' ), __( '% Comments', 'pho' ) );
 						echo '</span>';
@@ -124,17 +124,17 @@
 				</div><!-- .entry-meta -->
 				<?php endif; ?>
 
-                
+
             </footer><!-- .entry-footer -->
         <?php } ?>
-        
+
 		</div> <!-- entry-box -->
-				
+
     </div> <!-- index-box -->
-    
+
 	<?php if (get_theme_mod( 'pho_show_post_separators' ) == 1 ) { ?>
 		<div class="separator">
     	</div>
     <?php } ?>
-	
+
 </article><!-- #post-## -->
