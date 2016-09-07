d6d6f5034493d117f2a2be5ec3d61499a1d64923
Markus Bauer <mkbauer@web.de>
2016-09-07 13:49:12 +0200

    More robust layout for portfolio elements (using masonry).
    This is includes Flickr media items introduced by the flickr-media-library plugin.
    This relies on picture cropping using CSS which changes picture sizes late in the page loading process and confuses masonry.

--- js/masonry-settings.js
+++ js/masonry-settings.js
@@ -15,43 +15,67 @@ jQuery(document).ready(function($){
                 isAnimated: true
             });
             $footer.imagesLoaded(function() {
-  				$footer.masonry();	
+  				$footer.masonry();
 			});
-        },      
+        },
 
-        // Triggered when the media query transitions 
+        // Triggered when the media query transitions
         // *from a matched state to an unmatched state*.
         unmatch : function() {
             $footer.masonry('destroy');
-        }   
-        
-    }); 
-    
+        }
+
+    });
+
     // Masonry settings to organize the portfolio
 	var $portfolio =$('.portfolio-grid');
+
 	if ($portfolio.length > 0) {
-		enquire.register("screen and (min-width:880px)", {
+
+        var refresh_portfolio = function() {
+            // Enforce square pictures by setting the height...
+            size = parseInt($('.portfolio-element').css('width'), 10)
+                 - parseInt($('.portfolio-element').css('padding-top'), 10)
+                 - parseInt($('.portfolio-element').css('padding-bottom'), 10);
+            $('.portfolio-element').each(function(){
+                $elem = $(this);
+                $elem.height(size);
+            });
+
+            // ...and letting masonry figuring out the width during layouting
+            $portfolio.masonry({
+				columnWidth: '.portfolio-element',
+				itemSelector: '.portfolio-element',
+				// gutter: 40, // the widgets introduce some spacing on their own.
+				isFitWidth: false,
+				isAnimated: true
+			});
+        }
+
+
+		enquire.register("screen and (min-width:880px)", [{
 
 			// Triggered when a media query matches.
 			match : function() {
-				$portfolio.masonry({
-					columnWidth: '.portfolio-element',
-					itemSelector: '.portfolio-element',
-					// gutter: 40, // the widgets introduce some spacing on their own.
-					isFitWidth: false,
-					isAnimated: true
-				});
-				$portfolio.imagesLoaded(function() {
-					$portfolio.masonry();	
-				});
-			},      
+                refresh_portfolio();
 
-			// Triggered when the media query transitions 
+                // Resizing the browser leads to a new layout
+                $(window).resize(refresh_portfolio);
+
+			},
+
+			// Triggered when the media query transitions
 			// *from a matched state to an unmatched state*.
+            // Remove masonry and reset height of portfolio boxes
 			unmatch : function() {
 				$portfolio.masonry('destroy');
-			}   
-		
-		}); 
+                $('.portfolio-element').each(function(){
+                    $elem = $(this);
+                    $elem.css('height', 'auto');
+                });
+                $(window).off('resize', refresh_portfolio);
+			}
+
+		}]);
     }
 });
